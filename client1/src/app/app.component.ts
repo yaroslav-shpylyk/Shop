d8950e0109218725971f928fb1063d8bc8add73f
Yaroslav Shpylyk <yshpylyk@roku.com>
2022-06-05 09:40:55 +0300

    move client into new folder

---
+++ client1/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  constructor(private httpClient: HttpClient) {}
+
+  getItems() : void {
+    this.httpClient.get('http://localhost:80/api/items').subscribe();
+  }
+}
