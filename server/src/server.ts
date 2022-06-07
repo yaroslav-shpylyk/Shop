070aba6b0b2a213c3df2006cf0142ed75a0c263b
Yaroslav Shpylyk <yshpylyk@roku.com>
2022-06-07 15:56:30 +0300

    add error handling

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,11 +3,11 @@ import pool from './connector';
 import bodyParser from 'body-parser';
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = 8080;
 
 pool.connect()
-  .then(() => console.log('DB connected'))
-  .catch(() => console.log('Failed to connect DB'));
+  .then(() => console.log('DB connection success'))
+  .catch(() => console.log('DB connection fail'));
 
 app.use(express.json());
 app.use(bodyParser.json());
@@ -15,16 +15,38 @@ app.use(bodyParser.json());
 app.post('/object', async (req, res) => {
   const client = await pool.connect();
   const {name, type} = req.body;
-  await client.query('INSERT INTO objects (name, type) VALUES ($1, $2)', [name, type]);
-  client.release();
-  res.status(201).send();
+  try {
+    await client.query('INSERT INTO objects (name, type) SELECT $1, $2 WHERE NOT EXISTS (SELECT 1 FROM objects WHERE name=$3)', [name, type, name]);
+    res.status(201).send();
+  } catch {
+    res.status(404).send();
+  } finally {
+    client.release();
+  }
+})
+
+app.delete('/object/:name', async (req, res) => {
+  const client = await pool.connect();
+  try {
+    await client.query('DELETE FROM objects WHERE name=$1', [req.params.name]);
+    res.status(204).send();
+  } catch {
+    res.status(404).send();
+  } finally {
+    client.release();
+  }
 })
 
 app.get('/objects', async (req, res) => {
   const client = await pool.connect();
-  const items = await client.query('SELECT * FROM objects');
-  client.release();
-  res.json(items.rows);
+  try {
+    const items = await client.query('SELECT * FROM objects');
+    res.json(items.rows);
+  } catch {
+    res.status(404).send();
+  } finally {
+    client.release();
+  }
 });
 
 app.listen(port, () => console.log(`Started on port: ${port}`));
