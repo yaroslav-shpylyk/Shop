f96349c8f86cb09b2e1f3257c648e8f6ff1a4ccc
Yaroslav Shpylyk <yshpylyk@roku.com>
2022-06-26 09:26:07 +0300

    add payload interface

--- client/src/app/interfaces/animal.interface.ts
+++ client/src/app/interfaces/animal.interface.ts
@@ -1,5 +1,8 @@
-export interface IAnimal {
-  id: number;
+export interface IAnimalPayload {
   name: string;
   type: string;
+}
+
+export interface IAnimal extends IAnimalPayload {
+  id: number;
 }
