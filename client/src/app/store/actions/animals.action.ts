636a5668b221a44dd3736812706a149963cd044b
Yaroslav Shpylyk <yshpylyk@roku.com>
2022-06-26 09:33:21 +0300

    make shorter action names

--- client/src/app/store/actions/animals.action.ts
+++ client/src/app/store/actions/animals.action.ts
@@ -1,11 +1,12 @@
 import { createAction, props } from '@ngrx/store';
 
 import { IAnimal } from '../../interfaces/animal.interface';
+import { IProps } from '../../interfaces/props.interface';
 
 enum EAnimalsActions {
-  GET_ANIMALS = '[Animals] Get Animals',
-  GET_ANIMALS_SUCCESS = '[Animals] Get Animals Success'
+  GET = '[Animals] Get',
+  GET_SUCCESS = '[Animals] Get Success'
 }
 
-export const getAnimals = createAction(EAnimalsActions.GET_ANIMALS);
-export const getAnimalsSuccess = createAction(EAnimalsActions.GET_ANIMALS_SUCCESS, props<{payload: IAnimal[]}>());
+export const getAnimals = createAction(EAnimalsActions.GET);
+export const getAnimalsSuccess = createAction(EAnimalsActions.GET_SUCCESS, props<IProps<IAnimal[]>>());
